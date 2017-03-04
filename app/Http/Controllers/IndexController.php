6c0d94fbda7106a12e0029a92167de5447e295f9
alex <camward@yandex.ru>
2017-03-04 17:18:52 +0400

    Модели. Разделение шаблона php artisan make:model Page

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,9 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Page;
+use App\Portfolio;
+use App\Service;
+use App\People;
+
 class IndexController extends Controller
 {
     public function execute(Request $request){
-        return view('layouts.site');
+        $pages = Page::all(); // все записи и все поля
+        $portfolios = Portfolio::get(['name', 'filter', 'images']); // все записи, но определенные поля
+        $services = Service::where('id', '<', 20)->get(); // записи, у которых ид меньше 20
+        $peoples = People::take(3)->get(); // только 3 записи
+
+        return view('site.index');
     }
 }
