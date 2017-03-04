dfc79e599d73794d963692d4733e1a22aa0bbf2c
alex <camward@yandex.ru>
2017-03-04 23:55:17 +0400

    Детальная страница

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Page;
+
+class PageController extends Controller
+{
+    public function execute($alias){
+        if(!$alias){
+            abort(404); // генерируем исключение
+        }
+
+        if(view()->exists('site.page')){
+            $page = Page::where('alias',strip_tags($alias))->first();
+            $data = [
+                'title'=>$page->name,
+                'page'=>$page
+            ];
+            return view('site.page', $data);
+        }
+    }
+}
