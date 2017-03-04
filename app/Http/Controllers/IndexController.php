2e1ecac4412892f4ff7d2614368e6831159822ae
alex <camward@yandex.ru>
2017-03-04 23:18:00 +0400

    Отправка формы

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,10 +9,39 @@ use App\Portfolio;
 use App\Service;
 use App\People;
 use DB;
+use Mail;
 
 class IndexController extends Controller
 {
     public function execute(Request $request){
+
+        if($request->isMethod('post')){
+            // свои сообщения для валидации
+            $messages = [
+                'required'=>'Поле :attribute обязательно к заполнению',
+                'email'=>'Поле :attribute не соответствует email-адресу'
+            ];
+
+            // валидация
+            $this->validate($request, [
+                'name'=>'required|max:255',
+                'email'=>'required|email',
+                'text'=>'required'
+            ], $messages);
+
+            // отправка писем
+            $data = $request->all();
+            $result = Mail::send('site.email', ['data'=>$data], function($message) use ($data){
+                $mail_admin = env('MAIL_ADMIN');
+                $message->from($data['email'], $data['name']);
+                $message->to($mail_admin, 'Mr. Admin')->subject('Сообщение с сайта');
+            });
+
+            if($result){
+                return redirect()->route('home')->with('status', 'Сообщение отправлено');
+            }
+        }
+
         $pages = Page::all(); // все записи и все поля
         $portfolios = Portfolio::get(['name', 'filter', 'images']); // все записи, но определенные поля
         $services = Service::where('id', '<', 20)->get(); // записи, у которых ид меньше 20
